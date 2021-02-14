8b9fb6edd781e66b571c026ee564daeb7999686a
Kundan Kumar <rockingkundan26@gmail.com>
2021-02-14 23:43:19 +0530

    new on1e

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
 server.listen(port, ()=>{
     console.log("server started");
